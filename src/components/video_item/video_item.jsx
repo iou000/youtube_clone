6571881807710fb1bda220b430bff771d9920ffb
iou000 <iou000@naver.com>
2021-06-20 04:35:42 +0900

    비디오리스트(썸네일, 제목, 채널만) 생성

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import styles from './video_item.module.css';
+
 
 const VideoItem = (props) => {           
 
     return(
-        <li>{props.video.snippet.title}</li>
+        <li className={styles.container}>
+        <div className={styles.video}>
+            <img className={styles.thumbnail}
+            src={props.video.snippet.thumbnails.medium.url} 
+            alt="video thumbnail" />
+
+            <div className={styles.details}>
+                <p className={styles.title}>{props.video.snippet.title}</p>
+                <p className={styles.channel}>{props.video.snippet.channelTitle}</p>
+            </div>
+
+        </div>
+        </li>
     )
 };
 
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styles from './video_list.module.css';
 
 const VideoList = (props) => {
 
     return (
-    <ul>
+    <ul className={styles.videos}>
         {props.videos.map(video => {  //props(videos) 배열을 돌면서 vidoe에 있는 items(배열 원소)를 VideoItem컴포넌트로 만들어줌.
             return <VideoItem key={video.id} video={video}/>
         })}
