8d9c7fdda6b45a53930dccf4d2f92f34d65096ff
iou000 <iou000@naver.com>
2021-06-23 16:36:50 +0900

    검색 기능 구현

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState }from 'react';
-import './app.css';
+import styles from './app.module.css';
+import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const[videos, setVideos] = useState([]);
+
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    // query= input.current.value 유튜브에서 query에 맞는 영상들의 데이터를 받아옴.
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDqC6RvlhzrU4wupY19pMYO6tCTTMIPqPA`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  }
 
   useEffect(() => {
     const requestOptions = {
@@ -23,12 +37,11 @@ function App() {
 
 
 
-
-
   return (
-    <>
+    <div className={styles.app}>
+    <Search_header onSearch={search}/>
     <VideoList videos={videos}/>
-    </>
+    </div>
   );
 }
 
