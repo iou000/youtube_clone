a4ffd57c4ae543d629321a25b738f61aa6177c9e
iou000 <iou000@naver.com>
2021-06-25 01:41:52 +0900

    검색기능 리팩토링

--- src/app.jsx
+++ src/app.jsx
@@ -3,35 +3,17 @@ import styles from './app.module.css';
 import Search_header from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtubeService }) {
   const[videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    // query= input.current.value 유튜브에서 query에 맞는 영상들의 데이터를 받아옴.
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyDqC6RvlhzrU4wupY19pMYO6tCTTMIPqPA`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtubeService.searchVideos(query)
+    .then(videos => setVideos(videos));
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    //유튜브에서 가장 유명한 영상들의 데이터를 받아옴.
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDqC6RvlhzrU4wupY19pMYO6tCTTMIPqPA",
-     requestOptions
-     )
-      .then(response => response.json())
-      .then(result =>  setVideos(result.items))  //videos(처음엔 빈 배열)를 response의 items로 업데이트
-      .catch(error => console.log('error', error));
-
+    youtubeService.mostPopular()
+    .then(videos => setVideos(videos));
   },[]);
 
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
+import YoutubeService from './components/service/youtubeService';
+
+const youtubeService = new YoutubeService(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtubeService={youtubeService}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
